f3a84ba6dc9e1a92dea44ce16fd8e290cbfe04b4
Anatoliy <suddenley@bk.ru>
2023-02-20 13:51:52 +0300

    add more func

--- sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
@@ -3,7 +3,26 @@ package ru.stqa.pft.sandbox;
 public class First{
 	
 	public static void main (String[] args) {
-		System.out.println("Hello, world!");
+		hello("world");
+		hello("user");
+		hello ("alexey");
+
+		double l = 5;
+		System.out.println("P квадрата со стороной " + l + " = " + area(1));
+		double a = 4;
+		double b = 6;
+		System.out.println("P прямоугольника со стоонами " + a + " и " + b + " = " + area(a, b));
 	}
 	
+	public static void hello (String somebody) {
+		System.out.println("Hello, " + somebody +"!");
+	}
+
+	public static double area (double len){
+		return len * len;
+	}
+
+	public static double area (double a, double b){
+		return a * b;
+	}
 }
