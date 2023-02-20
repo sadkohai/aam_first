31dbf13d31995d842a992050d943c18ee309adb2
Anatoliy <suddenley@bk.ru>
2023-02-20 14:12:19 +0300

    Upd (перед заданием 2)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/First.java
@@ -7,22 +7,15 @@ public class First{
 		hello("user");
 		hello ("alexey");
 
-		double l = 5;
-		System.out.println("P квадрата со стороной " + l + " = " + area(1));
-		double a = 4;
-		double b = 6;
-		System.out.println("P прямоугольника со стоонами " + a + " и " + b + " = " + area(a, b));
+		Square s = new Square(5);
+		System.out.println("P квадрата со стороной " + s.l + " = " + s.area());
+
+		Rectangle r = new Rectangle(4,6);
+		System.out.println("P прямоугольника со стоонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 	
 	public static void hello (String somebody) {
 		System.out.println("Hello, " + somebody +"!");
 	}
 
-	public static double area (double len){
-		return len * len;
-	}
-
-	public static double area (double a, double b){
-		return a * b;
-	}
 }
